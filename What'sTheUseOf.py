1709b12a392949ff76dc2d894fc964a3b6fc5726
jamigeo <115463184+jamigeo@users.noreply.github.com>
2022-10-20 15:00:09 +0200

    Create What'sTheUseOf.py

---
+++ What'sTheUseOf.py
@@ -0,0 +1,40 @@
+import math
+
+num = int(input("Please enter number: "))
+
+class Prime:
+
+    i: int = 7
+
+def isPrime(self, num):
+
+    if num < 0:
+
+        print(f"{num} isn't positiv")
+
+    else:
+
+        sum = [], []
+  
+        while(num > 0):
+
+            sum += num
+            num -= 1
+
+    for i in[ 
+
+    range(2,int(math.sqrt(num))+1) == {
+
+        num != 1 and num != 0 == {
+
+            num % i == {
+
+                self.isPrime()
+            } 
+
+        }
+        
+    }
+]: print(i)
+
+prime = Prime()
